405ab662710c88b8ac56d1cc1e20a41184543f06
Michael Vlamis <michael.vlamis@icloud.com>
2024-09-17 11:50:02 -0400

    movement variables exercise

---
+++ movement-variables/js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 1,
+        y: -2
+    },
+    minVelocity: {
+        x: -3,
+        y: -2
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2
+    },
+    acceleration: {
+        x: 0.025,
+        y: -0.05
+    }
+}
+
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    background(0);
+
+    bird.velocity.x = bird.velocity.x + bird.acceleration.x;
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    // velocities to control direction
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+
+    // Draw the bird
+    ellipse(bird.x, bird.y, bird.size);
+}
