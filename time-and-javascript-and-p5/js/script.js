1f26ef41fd853eb510bed2e15444d1d5fd26fab3
Michael Vlamis <michael.vlamis@icloud.com>
2024-10-01 11:46:36 -0400

    time and javascript

---
+++ time-and-javascript-and-p5/js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const ball = {
+    x: 0,
+    y: 200,
+    size: 50
+};
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0);
+
+    ball.x += 1;
+
+    push();
+    noStroke();
+    ellipse(ball.x, ball.y, ball.size);
+    pop();
+}
