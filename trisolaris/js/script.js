96d910159be5dc38147a7804d6e7a0893bbb552a
Michael Vlamis <michael.vlamis@icloud.com>
2024-10-22 11:38:22 -0400

    Functions with parameters and functions with return values

---
+++ trisolaris/js/script.js
@@ -0,0 +1,35 @@
+/**
+ * Trisolaris
+ * Pippin
+ * 
+ * Draws the three suns of Trisolaris. Poorly.
+ */
+
+"use strict";
+
+/**
+ * Create the canvas
+ */
+function setup() {
+    createCanvas(600, 400);
+}
+
+/**
+ * Draw the three suns
+ */
+function draw() {
+    // Sky blue
+    background("#87ceeb");
+
+    drawSun(500, 100, 80);
+    drawSun(350, 180, 200);
+    drawSun(120, 100, 160);
+}
+
+function drawSun(x, y, size) {
+    push();
+    noStroke();
+    fill("#f99736");
+    ellipse(x, y, size);
+    pop();
+}
